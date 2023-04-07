415e0dcedaacd0aeda32e5b389128f619d2c1f97
jkbmrz <jakob.mraz@gmail.com>
2023-04-07 12:26:25 +0200

    restructure returns to offer info required for automatic loading

--- src/luxonis_ml/ops/dataset_recognition.py
+++ src/luxonis_ml/ops/dataset_recognition.py
@@ -6,31 +6,6 @@ from pathlib import Path
 import xml.etree.ElementTree as ET
 import pandas as pd
 
-## image formats supported by cv2
-IMAGE_EXTENSIONS = [
-    "bmp", 
-    "dib",
-    "jpeg",
-    "jpg",
-    "jpe",
-    "jp2",
-    "png",
-    "WebP",
-    "webp",
-    "pbm",
-    "pgm",
-    "ppm",
-    "pxm",
-    "pnm",
-    "sr",
-    "ras",
-    "tiff",
-    "tif",
-    "exr",
-    "hdr",
-    "pic"
-    ]
-
 def list_files(root="", extensions=[]):
     file_paths = []
     for path, subdirs, files in os.walk(root):
@@ -39,7 +14,30 @@ def list_files(root="", extensions=[]):
     return [file_path for file_path in file_paths if file_path.split(".")[-1] in extensions]
 
 def list_all_image_files(root):
-    return list_files(root, IMAGE_EXTENSIONS)
+    cv2_supported_image_formats = [
+        "bmp", 
+        "dib",
+        "jpeg",
+        "jpg",
+        "jpe",
+        "jp2",
+        "png",
+        "WebP",
+        "webp",
+        "pbm",
+        "pgm",
+        "ppm",
+        "pxm",
+        "pnm",
+        "sr",
+        "ras",
+        "tiff",
+        "tif",
+        "exr",
+        "hdr",
+        "pic"
+    ]
+    return list_files(root, cv2_supported_image_formats)
 
 def list_all_json_files(root):
     return list_files(root, ["json"])
@@ -59,11 +57,8 @@ def list_all_yaml_files(root):
 def list_all_csv_files(root):
     return list_files(root, ["csv"]) 
 
-def find_path(file_name, file_paths):
-    for file_path in file_paths:
-        if file_path.endswith(file_name):
-            return file_path.replace(file_name,"")
-    return None
+def get_unique_paths(file_paths):
+    return list(set([os.path.split(file_path)[0] for file_path in file_paths]))
 
 # TODO: place for common parser args
 def recognize(dataset_path: str) -> str:
@@ -80,7 +75,6 @@ def recognize(dataset_path: str) -> str:
 
     ## get dataset characteristics
     image_files = list_all_image_files(dataset_path)
-    image_names = [os.path.split(image_file)[-1] for image_file in image_files]
     json_files = list_all_json_files(dataset_path)
     xml_files = list_all_xml_files(dataset_path)
     txt_files = list_all_txt_files(dataset_path)
@@ -88,12 +82,27 @@ def recognize(dataset_path: str) -> str:
     yaml_files = list_all_yaml_files(dataset_path)
     csv_files = list_all_csv_files(dataset_path)
 
+    image_names = [os.path.split(image_file)[-1] for image_file in image_files]
+
+    image_extensions = set([image_file.split(".")[-1] for image_file in image_files])
+    
     n_of_images = len(image_files)
     if n_of_images == 0:
         if tfrecord_files:
             pass # images packed in the TFRECORD file
         else:
             raise Exception("No images found.")
+
+    image_dirs = get_unique_paths(image_files)  
+    if len(image_dirs) > 1:
+        if not any ([json_files, xml_files, txt_files, tfrecord_files, yaml_files, csv_files]):
+            return "ClassificationDirectoryTree", {"image_dirs": image_dirs}
+        else:
+            return "multiple directories with image files - possible ambiguity."
+    
+    xml_dirs = get_unique_paths(xml_files)
+    if len(xml_dirs) > 1:
+        return "multiple directories with xml files - possible ambiguity."
 
     ## Recognition based on JSON - COCO, FiftyOneImageDetection, and CreateML data formats
     if json_files:
@@ -107,23 +116,13 @@ def recognize(dataset_path: str) -> str:
         except ValueError:
             raise Exception(f"{json_file} is not a valid json file.")
 
-        annotations_path = os.path.split(json_files[0])[0]
-        image_paths = []
-
         if isinstance(json_file, dict):
             if all(key in json_file for key in ['info', 'licenses', 'images', 'annotations', 'categories']):
                 for image in json_file["images"]:
                     image_name = image["file_name"]
-                    image_path = find_path(image_name, image_files)
-                    if image_path == None:
+                    if image_name not in image_names:
                         return "unmatching json annotations"
-                    image_paths.append(image_path)
-                
-                if len(set(image_paths)) > 1:
-                    return "multiple image paths - possible ambiguity."
-                images_path = image_paths[0]
-
-                return "COCO", {"images_path": images_path, "annotations_path": annotations_path}
+                return "COCO", {"image_dir": image_dirs[0], "json_file_path": json_files[0]}
             
             if all(key in json_file for key in ['classes', 'labels']):
                 if False: # TODO
@@ -134,86 +133,72 @@ def recognize(dataset_path: str) -> str:
             if all(key in json_file[0] for key in ["image", "annotations"]):
                 for data_instance in json_file:
                     image_name = data_instance["image"]
-                    image_path = find_path(image_name, image_files)
-                    if image_path == None:
+                    if image_name not in image_names:
                         return "unmatching json annotations"
-                    image_paths.append(image_path)
-                
-                if len(set(image_paths)) > 1:
-                    return "multiple image paths - possible ambiguity."
-                images_path = image_paths[0]
-
-                return "CreateML", {"images_path": images_path, "annotations_path": annotations_path}
+                return "CreateML", {"image_dir": image_dirs[0], "json_file_path": json_files[0]}
 
     ## Recognize based on XML - PascalVOC data format
     if xml_files:
-        image_paths = []
-        annotation_paths = []
-
         for xml_file in xml_files:
             instance_tree = ET.parse(xml_file)
             instance_root = instance_tree.getroot()
             for child in instance_root:
                 if child.tag == "filename":
                     image_name = child.text
-                    image_path = find_path(image_name, image_files)
-                    if image_path == None:
+                    if image_name not in image_names:
                         return "unmatching xml annotations"
-                    image_paths.append(image_path)
-            annotation_paths.append(os.path.split(xml_file)[0])
-                
-        if len(set(image_paths)) > 1:
-            return "multiple image paths - possible ambiguity."
-        images_path = image_paths[0]
 
-        if len(set(annotation_paths)) > 1:
-            return "multiple image paths - possible ambiguity."
-        annotations_path = annotation_paths[0]
-
-        return "VOC", {"images_path": images_path, "annotations_path": annotations_path}
+        return "VOC", {"image_dir": image_dirs[0], "xml_files_paths": xml_files}
 
     ## Recognize based on TXT - YOLO4, YOLO5, and KITTY data formats
-    for txt_file in txt_files:
-        with open(txt_file, 'r', encoding='utf-8-sig') as text:
-            possible_formats = []
-            for line in text.readlines():
-                line_split = line.split(" ")
-                
-                if len(line_split) == 2:
-                    if line_split[0] in image_names:
-                        if line_split[1].split(",")[0] == line_split[1]:
-                            possible_formats.append("ClassificationWithTextAnnotations")
-                        else:
-                            possible_formats.append("YOLO4")
-                
-                elif len(line_split) > 2:
-                    if line_split[0] in image_names:
-                        possible_formats.append("YOLO4")
-                    else:
-                        possible_formats.append("YOLO5 or KITTY")
+    if txt_files:
+        possible_dataset_types = []
+        relevant_txt_files = [] # exclude README files etc.
+        for txt_file in txt_files:
+            relevant_txt_file = False
 
-            if len(set(possible_formats)) == 1:
-                return possible_formats[0]
+            for ext in image_extensions:
+                if os.path.split(txt_file)[1].replace(".txt", f".{ext}") in image_names:
+                    possible_dataset_types.append("YOLO5 or KITTY")
+                    relevant_txt_file = True
+
+            with open(txt_file, 'r', encoding='utf-8-sig') as text:
+                for line in text.readlines():
+                    line_split = line.split(" ")
+                    if line_split[0] in image_names:
+                        if len(line_split) == 2:
+                                if line_split[1].split(",")[0] == line_split[1]:
+                                    possible_dataset_types.append("ClassificationWithTextAnnotations")
+                                else:
+                                    possible_dataset_types.append("YOLO4")
+                        elif len(line_split) > 2:
+                                possible_dataset_types.append("YOLO4")
+                        relevant_txt_file = True
+
+            if relevant_txt_file:
+                relevant_txt_files.append(txt_file)
+            
+        if len(set(possible_dataset_types)) == 1:
+            return possible_dataset_types[0], {"image_dir": image_dirs[0], "txt_annotation_files_paths": relevant_txt_files}
 
     ## Recognize based on TFRECORD - TFObjectDetectionDataset
     if tfrecord_files:
         if len(json_files) > 1:
             raise Exception("Multiple TFRECORD files present - possible ambiguity.")
         else:
-            return "TFObjectDetectionDataset"
+            return "TFObjectDetectionDataset", {"tfrecord_file_path": tfrecord_files[0]}
 
     ## Recognize based on CSV - TFObjectDetectionCSV
     if csv_files:
-        for csv_file in csv_files:
-            for instance_name in pd.read_csv(csv_file)["filename"]:
-                if instance_name not in image_names:
-                    return "unmatching csv annotations"
-        return "TFObjectDetectionCSV"
 
-    ## No characteristic files
-    if not json_files and not xml_files and not txt_files:
-        if not tfrecord_files and not yaml_files and not csv_files:
-            return "ClassificationDirectoryTree"
+        if len(csv_files) > 1:
+            raise Exception("Multiple CSV files present - possible ambiguity.")
+
+        for instance_name in pd.read_csv(csv_files[0])["filename"]:
+            if instance_name not in image_names:
+                return "unmatching csv annotations"
+        
+        return "TFObjectDetectionCSV", {"image_dir": image_dirs[0], "csv_file_path": csv_files[0]}
 
     ## Recognize based on YAML files
     # TODO - legacy code - not sure what this detects
