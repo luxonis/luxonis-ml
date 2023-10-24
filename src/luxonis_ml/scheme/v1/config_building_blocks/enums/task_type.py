e9f1a25616b6189985aec11dadad4ff90dc6dbe7
jkbmrz <jakob.mraz@gmail.com>
2023-10-24 18:20:41 +0200

    simplify task types enum

--- src/luxonis_ml/scheme/v1/config_building_blocks/enums/task_type.py
+++ src/luxonis_ml/scheme/v1/config_building_blocks/enums/task_type.py
@@ -4,12 +4,12 @@ class TaskType(Enum):
 
     """ all existing model task types """
 
-    CLASSIFICATION = "classification" # ?
-    MULTICLASS_CLASSIFICATION = "multiclass_classification" # e.g. male-blue, female-blue, male-orange, female-orange - 4 labels
-    MULTILABEL_CLASSIFICARION = "multilabel_classification" # e.g. male/female and the other blue/orange - 2 labels
-    OBJECT_DETECTION = "object_detection" # ?
-    SEMANTIC_SEGMENTATION = "semantic_segmentation" # ?
-    INSTANCE_SEGMENTATION = "instance segmentation"
+    CLASSIFICATION = "classification"
+    #MULTICLASS_CLASSIFICATION = "multiclass_classification"
+    #MULTILABEL_CLASSIFICARION = "multilabel_classification"
+    OBJECT_DETECTION = "object_detection"
+    SEGMENTATION = "segmentation"
+    #SEMANTIC_SEGMENTATION = "semantic_segmentation"
+    #INSTANCE_SEGMENTATION = "instance segmentation"
     KEYPOINT_DETECTION = "keypoint_detection"
-    MISC = "misc"
     RAW = "raw"
