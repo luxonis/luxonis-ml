fc5989642ee46bb42c9dd2cc7c71d4fbcce6e1f4
jkbmrz <jakob.mraz@gmail.com>
2023-10-28 18:20:22 +0200

    add ssd-mobilenet decoding family

--- src/luxonis_ml/scheme/v1/config_building_blocks/enums/decoding.py
+++ src/luxonis_ml/scheme/v1/config_building_blocks/enums/decoding.py
@@ -5,6 +5,7 @@ class DecodingFamily(Enum):
     """ members of a family should have similar decoding """
 
     YOLO = "yolo"
+    SSD_MB = "ssd-mobilenet" # seems like outputs of all ssd-mobilenet networks are of shape (1,3000,classes_n) and (1,3000,4)
     # TODO: add additional families
     RAW = "raw"
 
@@ -16,7 +17,6 @@ class DecodingSubFamily(Enum):
     # however, outputs are structured a bit differently:
     YOLOv5_7 = "yolov5/yolov7" # both (1, 3, 80, 80, 85), (1, 3, 40, 40, 85), (1, 3, 20, 20, 85) shape outputs, as the (1, 25200, 85) concatenate
     YOLOv6 = "yolov6" # (1, 80, 80, 85), (1, 40, 40, 85), (1, 20, 20, 85) shape outputs
-    #YOLOv3?
-    
+    #YOLOv3?    
     # TODO: add additional sub-families
     RAW = "raw"
