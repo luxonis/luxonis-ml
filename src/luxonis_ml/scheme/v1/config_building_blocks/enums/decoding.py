05ef9466c0e51f5ad304482df334343339836ba4
jkbmrz <jakob.mraz@gmail.com>
2023-10-30 16:28:21 +0100

    add softmax decoding family

--- src/luxonis_ml/scheme/v1/config_building_blocks/enums/decoding.py
+++ src/luxonis_ml/scheme/v1/config_building_blocks/enums/decoding.py
@@ -6,8 +6,10 @@ class DecodingFamily(Enum):
 
     YOLO = "yolo"
     SSD_MB = "ssd-mobilenet" # seems like outputs of all ssd-mobilenet networks are of shape (1,3000,classes_n) and (1,3000,4)
-    # TODO: add additional families
-    RAW = "raw"
+    
+    SOFTMAX= "softmax"
+    
+    RAW = "raw" # TODO: add additional families
 
 class DecodingSubFamily(Enum):
 
@@ -17,6 +19,5 @@ class DecodingSubFamily(Enum):
     # however, outputs are structured a bit differently:
     YOLOv5_7 = "yolov5/yolov7" # both (1, 3, 80, 80, 85), (1, 3, 40, 40, 85), (1, 3, 20, 20, 85) shape outputs, as the (1, 25200, 85) concatenate
     YOLOv6 = "yolov6" # (1, 80, 80, 85), (1, 40, 40, 85), (1, 20, 20, 85) shape outputs
-    #YOLOv3?    
-    # TODO: add additional sub-families
-    RAW = "raw"
+    
+    RAW = "raw" # TODO: add additional sub-families
