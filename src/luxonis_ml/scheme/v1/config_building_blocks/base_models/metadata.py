a77dbed39f1a668ecc16c2f8c5102587ff9a6b1c
jkbmrz <jakob.mraz@gmail.com>
2023-11-02 12:31:53 +0100

    reduce metadata fields restrictions

--- src/luxonis_ml/scheme/v1/config_building_blocks/base_models/metadata.py
+++ src/luxonis_ml/scheme/v1/config_building_blocks/base_models/metadata.py
@@ -1,7 +1,7 @@
 from .custom_base_model import CustomBaseModel
 from ..enums import *
 
-class Metadata(CustomBaseModel):
+class Metadata(BaseModel):
     name: str
     backbone: str
     platform: Platform
