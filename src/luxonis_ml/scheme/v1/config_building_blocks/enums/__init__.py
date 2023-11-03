601670ff168c988d24acf5da07e784d30483ca7e
jkbmrz <jakob.mraz@gmail.com>
2023-11-03 15:19:27 +0100

    remove task type

--- src/luxonis_ml/scheme/v1/config_building_blocks/enums/__init__.py
+++ src/luxonis_ml/scheme/v1/config_building_blocks/enums/__init__.py
@@ -1,5 +1,4 @@
 from .data_type import DataType
 from .input_type import InputType
 from .platform import Platform
-from .decoding import DecodingFamily, DecodingSubFamily
-from .task_type import TaskType
+from .decoding import DecodingFamily, ObjectDetectionSubtype
