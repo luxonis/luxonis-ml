c69223fe1556e991e8a9f0f9d6420819ef3b5e50
jkbmrz <jakob.mraz@gmail.com>
2023-11-13 17:06:26 +0100

    removed DecodingFamily add the SSD subtype

--- src/luxonis_ml/scheme/v1/config_building_blocks/enums/decoding.py
+++ src/luxonis_ml/scheme/v1/config_building_blocks/enums/decoding.py
@@ -1,29 +1,5 @@
 from enum import Enum
 
-class DecodingFamily(Enum):
-
-    """ 
-    Represents a decoding family. Members of a family should have similar decoding.
-    """
-
-    # classification
-    MULTICLASS_CLASSIFICATION= "multiclass_classification"
-
-    # object detection
-    YOLO = "yolo"
-    SSD_PARSED = "ssd-mobilenet-parsed" # seems like outputs of all ssd-mobilenet networks are of shape (1,3000,classes_n) and (1,3000,4)
-    
-    # keypoints
-    KEYPOINTS_HEATMAP = "heatmap"
-    KEYPOINTS_REGRESSION = "regression"
-    KEYPOINTS_ANCHORS = "anchors"
-    
-    # segmentation
-    # TODO
-
-    # misc
-    RAW = "raw"
-
 class ObjectDetectionSubtype(Enum):
 
     """ 
@@ -34,3 +10,5 @@ class ObjectDetectionSubtype(Enum):
     YOLOv6 = "yolov6"
     YOLOv7 = "yolov6"
     YOLOv8 = "yolov6"
+
+    SSD_PARSED = "ssd-mobilenet-parsed"
