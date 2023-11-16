f130d9b4fcbdbd58ed475a742aeac66d6a65c52b
jkbmrz <jakob.mraz@gmail.com>
2023-11-16 18:14:27 +0100

    adjust head metadata classes to better convey decoding families

--- src/luxonis_ml/scheme/v1/config_building_blocks/base_models/head.py
+++ src/luxonis_ml/scheme/v1/config_building_blocks/base_models/head.py
@@ -54,12 +54,42 @@ class HeadMetadataObjectDetection(HeadMetadata):
     iou_threshold: float
     conf_threshold: float
     max_det: int
-    subtype: ObjectDetectionSubtype
+
+    @validator("label_type")
+    def validate_label_type(
+        cls,
+        value,
+        ):
+        if value != LabelType.OBJECT_DETECTION:
+            raise ValueError("wrong HeadMetadata child class")
+        return value
+
+class HeadMetadataObjectDetectionYOLO(HeadMetadataObjectDetection):
+    """
+    Metadata for YOLO object detection head.
+
+    Attributes:
+        subtype (ObjectDetectionSubtypeYOLO): Determines YOLO family decoding subtype.
+        n_keypoints (int): Number of keypoints per bbox if provided.
+        n_prototypes (int): Number of prototypes per bbox if provided.
+        prototype_output_name (str): Output node containing prototype information.
+    """
+    subtype: ObjectDetectionSubtypeYOLO
     n_keypoints: Optional[int] = None
     n_prototypes: Optional[int] = None
     prototype_output_name: Optional[str] = None
-    anchors: Optional[List[List[int]]] = None # optional as some models (e.g. late versions of YOLO) use anchors as an integral part of their architecture.
     
+class HeadMetadataObjectDetectionSSD(HeadMetadataObjectDetection):
+    """
+    Metadata for SSD object detection head.
+
+    Attributes:
+        subtype (ObjectDetectionSubtypeYOLO): Determines SSD family decoding subtype.
+        anchors (list): Predefined bounding boxes of different sizes and aspect ratios.
+    """
+    subtype: ObjectDetectionSubtypeSSD
+    anchors: Optional[List[List[int]]] = None
+
 class HeadMetadataSegmentation(HeadMetadata):
     """
     Metadata for segmentation head. 
@@ -91,11 +121,9 @@ class Head(CustomBaseModel):
 
     Attributes:
         head_id (str): Unique head identifier.
-        decoding_family (DecodingFamily): Decoding family of the head.
         metadata: (HeadMetadata object): Parameters required by head to run postprocessing.
     """
     head_id: str
-    decoding_family: Optional[DecodingFamily] = None # optional because this is mostly relevant for object detection
     metadata: Union[
         HeadMetadataObjectDetection,
         HeadMetadataSegmentation,
--- src/luxonis_ml/scheme/v1/config_building_blocks/enums/__init__.py
+++ src/luxonis_ml/scheme/v1/config_building_blocks/enums/__init__.py
@@ -1,5 +1,5 @@
 from .data_type import DataType
 from .input_type import InputType
 from .platform import Platform
-from .decoding import DecodingFamily, ObjectDetectionSubtype
+from .decoding import ObjectDetectionSubtypeYOLO, ObjectDetectionSubtypeSSD
 from .label_type import LabelType
