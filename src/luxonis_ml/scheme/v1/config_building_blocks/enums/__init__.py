2fee269b0861cdc3d68f141bcd7e416fddef6d80
jkbmrz <jakob.mraz@gmail.com>
2023-11-21 18:35:58 +0100

    remove label_type enum

--- src/luxonis_ml/scheme/v1/config_building_blocks/enums/__init__.py
+++ src/luxonis_ml/scheme/v1/config_building_blocks/enums/__init__.py
@@ -2,4 +2,3 @@ from .data_type import DataType
 from .input_type import InputType
 from .platform import Platform
 from .decoding import ObjectDetectionSubtypeYOLO, ObjectDetectionSubtypeSSD
-from .label_type import LabelType
