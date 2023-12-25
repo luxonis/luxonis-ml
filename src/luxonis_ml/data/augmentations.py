00a8dcdf4ad90170d25bcd65c58db841b7908bdf
klemen1999 <klemenskrlj8@gmail.com>
2023-12-25 23:07:50 +0100

    added mixup augmentation

--- src/luxonis_ml/data/augmentations.py
+++ src/luxonis_ml/data/augmentations.py
@@ -696,3 +696,72 @@ class DeterministicMosaic4(A.Mosaic4):
         target_params = super().get_params_dependent_on_targets(params)
         target_params["indices"] = list(range(self.n_tiles))
         return target_params
+
+
+@AUGMENTATIONS.register_module()
+class MixUp(A.BatchBasedTransform):
+    def __init__(
+        self,
+        alpha: float = 0.5,
+        always_apply: bool = False,
+        p: float = 0.5,
+    ):
+        """MixUp augmentation that merges two images and their annotations into one.
+        If images are not of same size then second one is first resized to match the
+        first one.
+
+        Args:
+            alpha (float, optional): Alpha value used for blending images. Defaults to 0.5.
+            always_apply (bool, optional): Defaults to False.
+            p (float, optional): Probability of applying the transform. Defaults to 1.0.
+        """
+        super().__init__(always_apply=always_apply, p=p)
+
+        self.n_tiles = 2
+        self.alpha = alpha
+        self.out_batch_size = 1
+
+    def get_transform_init_args_names(self) -> Tuple[str, ...]:
+        return ("alpha", "out_batch_size")
+
+    @property
+    def targets_as_params(self):
+        return ["image_batch"]
+
+    def apply_to_image_batch(self, image_batch, image_shapes, **params):
+        image1 = image_batch[0]
+        # resize second image to size of the first one
+        image2 = cv2.resize(image_batch[1], (image_shapes[0][1], image_shapes[0][0]))
+        img_out = cv2.addWeighted(image1, self.alpha, image2, 1 - self.alpha, 0.0)
+        return [img_out]
+
+    def apply_to_mask_batch(self, mask_batch, image_shapes, **params):
+        mask1 = mask_batch[0]
+        mask2 = cv2.resize(
+            mask_batch[1],
+            (image_shapes[0][1], image_shapes[0][0]),
+            interpolation=cv2.INTER_NEAREST,
+        )
+        out_mask = mask1 + mask2
+        # if masks intersect keep one present in first image
+        mask_inter = mask1 > 0
+        out_mask[mask_inter] = mask1[mask_inter]
+        return [out_mask]
+
+    def apply_to_bboxes_batch(self, bboxes_batch, image_shapes, **params):
+        return [bboxes_batch[0] + bboxes_batch[1]]
+
+    def apply_to_keypoints_batch(self, keypoints_batch, image_shapes, **params):
+        scaled_kpts2 = []
+        scale_x = image_shapes[0][1] / image_shapes[1][1]
+        scale_y = image_shapes[0][0] / image_shapes[1][0]
+        for kpt in keypoints_batch[1]:
+            new_kpt = A.augmentations.geometric.functional.keypoint_scale(
+                keypoint=kpt, scale_x=scale_x, scale_y=scale_y
+            )
+            scaled_kpts2.append(new_kpt + kpt[4:])
+        return [keypoints_batch[0] + scaled_kpts2]
+
+    def get_params_dependent_on_targets(self, params: Dict[str, Any]) -> Dict[str, Any]:
+        image_batch = params["image_batch"]
+        return {"image_shapes": [image.shape[:2] for image in image_batch]}
