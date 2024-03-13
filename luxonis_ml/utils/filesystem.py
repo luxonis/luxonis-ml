5e1f0f99d56d1acfc50b398d6629e6e608032ff1
conorsim <bato.simmons@gmail.com>
2024-03-13 12:26:28 -0600

    fix(filesystem): tests for 3.8, 3.9

--- luxonis_ml/utils/filesystem.py
+++ luxonis_ml/utils/filesystem.py
@@ -331,7 +331,7 @@ class LuxonisFileSystem:
         if self.is_mlflow:
             raise NotImplementedError
         elif self.is_fsspec:
-            if isinstance(remote_paths, PathType):
+            if isinstance(remote_paths, Path) or isinstance(remote_paths, str):
                 self.fs.download(
                     str(self.path / remote_paths),
                     str(local_dir),
