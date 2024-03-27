4ecc4dbb2f2b63f70a786ec413a65c08b2076600
jkbmrz <jakob.mraz@gmail.com>
2024-03-27 09:56:30 +0100

    fix pre-commit formatting

--- luxonis_ml/data/datasets/luxonis_dataset.py
+++ luxonis_ml/data/datasets/luxonis_dataset.py
@@ -5,8 +5,8 @@ import os.path as osp
 import shutil
 import time
 from datetime import datetime
-from typing import Dict, List, Optional, Tuple
 from pathlib import Path
+from typing import Dict, List, Optional, Tuple
 
 import numpy as np
 import pandas as pd
