3f1855ffe3825dcdf4477e68fe5b52f0b62fb9cd
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-04-05 08:22:25 +0200

    docs

--- luxonis_ml/nn_archive/utils.py
+++ luxonis_ml/nn_archive/utils.py
@@ -5,6 +5,13 @@ from luxonis_ml.utils.filesystem import PathType
 
 
 def is_nn_archive(path: PathType) -> bool:
+    """Check if the given path is a valid NN archive file.
+
+    @type path: PathType
+    @param path: Path to the file to check.
+    @rtype: bool
+    @return: True if the file is a valid NN archive file, False otherwise.
+    """
     path = Path(path)
 
     if not path.is_file():
