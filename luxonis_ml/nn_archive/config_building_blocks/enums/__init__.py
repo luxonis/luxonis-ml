71aff54ae52c656a108ab7b1d79d6b7e55abe196
jkbmrz <jakob.mraz@gmail.com>
2024-05-30 10:20:20 +0200

    remove input layout import

--- luxonis_ml/nn_archive/config_building_blocks/enums/__init__.py
+++ luxonis_ml/nn_archive/config_building_blocks/enums/__init__.py
@@ -1,12 +1,10 @@
 from .data_type import DataType
 from .decoding import ObjectDetectionSubtypeSSD, ObjectDetectionSubtypeYOLO
-from .image_layout import ImageLayout
 from .input_type import InputType
 
 __all__ = [
     "DataType",
     "InputType",
-    "ImageLayout",
     "ObjectDetectionSubtypeSSD",
     "ObjectDetectionSubtypeYOLO",
 ]
