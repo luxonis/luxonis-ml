20b66d630e1d757dd94257aae29b781408b61548
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-07-03 17:26:53 +0200

    fix typo

--- luxonis_ml/nn_archive/config_building_blocks/base_models/head.py
+++ luxonis_ml/nn_archive/config_building_blocks/base_models/head.py
@@ -265,7 +265,7 @@ class HeadYOLO(HeadObjectDetection, HeadSegmentation, ABC):
 
         if len(tasks) == 0:
             raise ValueError(
-                "Invalid combination of parameters. No specific task can be infered."
+                "Invalid combination of parameters. No specific task can be inferred."
             )
 
         for param in defined_params:
