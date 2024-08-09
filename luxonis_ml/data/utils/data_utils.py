a339fa238515b60de5eb9df2d3e0e20cb0178523
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-08-09 18:07:50 +0200

    updated docs

--- luxonis_ml/data/utils/data_utils.py
+++ luxonis_ml/data/utils/data_utils.py
@@ -55,6 +55,9 @@ def rgb_to_bool_masks(
     @param segmentation_mask: An RGB segmentation mask where each pixel is colored according to the class it belongs to.
     @type class_colors: Dict[str, Tuple[int, int, int]]
     @param class_colors: A dictionary mapping class names to RGB colors.
+    @type add_background_class: bool
+    @param add_background_class: Whether to add a background class with a mask for all pixels
+        that do not belong to any class. The class name will be set to "background". Default is False.
     @rtype: Iterator[Tuple[str, npt.NDArray[np.bool_]]]
     @return: An iterator of tuples where the first element is the class name and
         the second element is a boolean mask for that class.
@@ -63,9 +66,7 @@ def rgb_to_bool_masks(
 
     lookup_table = np.zeros((256, 256, 256), dtype=np.uint8)
     for color, id in color_to_id.items():
-        lookup_table[color[0], color[1], color[2]] = (
-            id + 1
-        )  # +1 to reserve 0 for background
+        lookup_table[color[0], color[1], color[2]] = id + 1
 
     segmentation_ids = lookup_table[
         segmentation_mask[:, :, 0],
