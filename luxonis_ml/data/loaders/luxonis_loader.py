4e0155c3a554c9a497a70a656e426f98cb6e7e98
Jernej Sabadin <116955183+JSabadin@users.noreply.github.com>
2024-08-26 13:30:49 +0200

    Fixed path retrieval in loader which caused slow training on GCP (#159)

--- luxonis_ml/data/loaders/luxonis_loader.py
+++ luxonis_ml/data/loaders/luxonis_loader.py
@@ -213,7 +213,8 @@ class LuxonisLoader(BaseLoader):
             img_path = ann_rows[0][8]
         elif not self.stream:
             uuid = ann_rows[0][0]
-            img_path = next(self.dataset.media_path.glob(f"{uuid}.*"))
+            file_extension = ann_rows[0][8].rsplit(".", 1)[-1]
+            img_path = self.dataset.media_path / f"{uuid}.{file_extension}"
         else:
             # TODO: add support for streaming remote storage
             raise NotImplementedError(
