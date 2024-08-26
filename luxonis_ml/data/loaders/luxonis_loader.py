7cac916dc5418c8ca6f8b051852513a4b35f0d19
Jernej Sabadin <jernej14sabadin@gmail.com>
2024-08-26 08:46:23 +0200

    dummy loader

--- luxonis_ml/data/loaders/luxonis_loader.py
+++ luxonis_ml/data/loaders/luxonis_loader.py
@@ -2,15 +2,13 @@ import json
 import logging
 import random
 import warnings
-from collections import defaultdict
 from operator import itemgetter
 from typing import Dict, Optional, Tuple
 
-import cv2
 import numpy as np
 
 from ..augmentations import Augmentations
-from ..datasets import LuxonisDataset, load_annotation
+from ..datasets import LuxonisDataset
 from ..utils.enums import LabelType
 from .base_loader import BaseLoader, Labels, LuxonisLoaderOutput
 
@@ -195,52 +193,116 @@ class LuxonisLoader(BaseLoader):
         return img, out_dict  # type: ignore
 
     def _load_image_with_annotations(self, idx: int) -> Tuple[np.ndarray, Labels]:
-        """Loads image and its annotations based on index.
+        # """Loads image and its annotations based on index.
 
-        @type idx: int
-        @param idx: Index of the image
-        @rtype: Tuple[L{np.ndarray}, dict]
-        @return: Image as L{np.ndarray} in RGB format and a dictionary with all the
-            present annotations
-        """
+        # @type idx: int
+        # @param idx: Index of the image
+        # @rtype: Tuple[L{np.ndarray}, dict]
+        # @return: Image as L{np.ndarray} in RGB format and a dictionary with all the
+        #     present annotations
+        # """
 
-        ann_indices = self.idx_to_df_row[idx]
-        ann_rows = [self.df.row(row) for row in ann_indices]
-        if not self.dataset.is_remote:
-            img_path = ann_rows[0][8]
-        elif not self.stream:
-            uuid, file_extension = ann_rows[0][0], ann_rows[0][8].rsplit('.', 1)[-1]
-            img_path = self.dataset.media_path / f"{uuid}.{file_extension}"
-        else:
-            # TODO: add support for streaming remote storage
-            raise NotImplementedError(
-                "Streaming for remote bucket storage not implemented yet"
-            )
+        # ann_indices = self.idx_to_df_row[idx]
+        # ann_rows = [self.df.row(row) for row in ann_indices]
+        # if not self.dataset.is_remote:
+        #     img_path = ann_rows[0][8]
+        # elif not self.stream:
+        #     uuid, file_extension = ann_rows[0][0], ann_rows[0][8].rsplit('.', 1)[-1]
+        #     img_path = self.dataset.media_path / f"{uuid}.{file_extension}"
+        # else:
+        #     # TODO: add support for streaming remote storage
+        #     raise NotImplementedError(
+        #         "Streaming for remote bucket storage not implemented yet"
+        #     )
 
-        img = cv2.cvtColor(cv2.imread(str(img_path)), cv2.COLOR_BGR2RGB)
-        height, width, _ = img.shape
+        # img = cv2.cvtColor(cv2.imread(str(img_path)), cv2.COLOR_BGR2RGB)
+        # height, width, _ = img.shape
 
-        labels_by_task = defaultdict(list)
-        instance_counters = defaultdict(int)
-        for annotation_data in ann_rows:
-            _, _, type_, _, class_, instance_id, task, ann_str, _ = annotation_data
-            if instance_id < 0:
-                instance_counters[task] += 1
-                instance_id = instance_counters[task]
-            annotation = load_annotation(
-                type_,
-                ann_str,
-                {"class": class_, "task": task, "instance_id": instance_id},
-            )
-            labels_by_task[task].append(annotation)
+        # labels_by_task = defaultdict(list)
+        # instance_counters = defaultdict(int)
+        # for annotation_data in ann_rows:
+        #     _, _, type_, _, class_, instance_id, task, ann_str, _ = annotation_data
+        #     if instance_id < 0:
+        #         instance_counters[task] += 1
+        #         instance_id = instance_counters[task]
+        #     annotation = load_annotation(
+        #         type_,
+        #         ann_str,
+        #         {"class": class_, "task": task, "instance_id": instance_id},
+        #     )
+        #     labels_by_task[task].append(annotation)
 
-        labels: Labels = {}
-        for task, anns in labels_by_task.items():
-            assert anns, f"No annotations found for task {task}"
-            anns.sort(key=lambda x: x.instance_id)
-            array = anns[0].combine_to_numpy(
-                anns, self.class_mappings[task], width=width, height=height
-            )
-            labels[task] = (array, anns[0]._label_type)
+        # labels: Labels = {}
+        # for task, anns in labels_by_task.items():
+        #     assert anns, f"No annotations found for task {task}"
+        #     anns.sort(key=lambda x: x.instance_id)
+        #     array = anns[0].combine_to_numpy(
+        #         anns, self.class_mappings[task], width=width, height=height
+        #     )
+        #     labels[task] = (array, anns[0]._label_type)
 
+        # # return img, labels
+
+        img = np.random.randint(0, 256, (416, 416, 3), dtype=np.uint8)
+        labels = {}
+        labels[LabelType.BOUNDINGBOX] = np.array(
+            [
+                [
+                    3.00000000e00,
+                    2.98437500e-03,
+                    7.64000000e-02,
+                    9.52796875e-01,
+                    9.10117647e-01,
+                ],
+                [
+                    3.00000000e00,
+                    3.35765625e-01,
+                    3.41576471e-01,
+                    2.85015625e-01,
+                    4.71905882e-01,
+                ],
+                [
+                    3.00000000e00,
+                    7.44640625e-01,
+                    2.53929412e-01,
+                    2.55359375e-01,
+                    5.64047059e-01,
+                ],
+                [
+                    5.60000000e01,
+                    6.90875000e-01,
+                    6.81858824e-01,
+                    3.36718750e-02,
+                    3.91529412e-02,
+                ],
+                [
+                    5.60000000e01,
+                    8.44843750e-01,
+                    7.95505882e-01,
+                    9.00937500e-02,
+                    1.28823529e-01,
+                ],
+                [
+                    5.50000000e01,
+                    5.34234375e-01,
+                    2.89200000e-01,
+                    4.16343750e-01,
+                    4.83152941e-01,
+                ],
+                [
+                    5.60000000e01,
+                    3.11000000e-01,
+                    2.85270588e-01,
+                    8.31093750e-02,
+                    3.95058824e-02,
+                ],
+                [
+                    5.60000000e01,
+                    7.76765625e-01,
+                    7.26800000e-01,
+                    4.75468750e-02,
+                    4.99764706e-02,
+                ],
+            ]
+        )
         return img, labels
