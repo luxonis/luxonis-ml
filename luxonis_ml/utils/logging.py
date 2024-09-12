1dc5c43b549b01eedadb2ebf9260d35fd9bdb2d1
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-09-12 10:05:39 +0200

    added decorator to mark deprecated functions

--- luxonis_ml/utils/logging.py
+++ luxonis_ml/utils/logging.py
@@ -1,7 +1,8 @@
 import builtins
 import logging
 import warnings
-from typing import Optional
+from functools import wraps
+from typing import Dict, Optional, Type
 
 from rich import print as rprint
 from rich.console import Console
@@ -93,16 +94,97 @@ def setup_logging(
 
     logging.basicConfig(level=level, format=format, datefmt=datefmt, handlers=handlers)
 
+    def _custom_warning_handler(
+        message: str,
+        category: Type[Warning],
+        filename: str,
+        lineno: int,
+        _=None,
+        line: Optional[str] = None,
+    ):
+        logger = logging.getLogger(filename)
+        message = warnings.formatwarning(
+            message,
+            category,
+            filename,
+            lineno,
+            line,
+        )
+        logger.warning(message)
+
     if configure_warnings:
-        logger = logging.getLogger("warnings.warn")
-
-        def custom_warning_handler(message, *_):
-            logger.warning(message)
-
-        warnings.showwarning = custom_warning_handler
+        warnings.showwarning = _custom_warning_handler
 
 
 def reset_logging() -> None:
     """Resets the logging module back to its default state."""
     for handler in logging.root.handlers[:]:
         logging.root.removeHandler(handler)
+
+
+def deprecated(
+    *args: str,
+    suggested: Optional[Dict[str, str]] = None,
+    additional_message: Optional[str] = None,
+    altogether: bool = False,
+):
+    """Decorator to mark a function or its parameters as deprecated.
+
+    Example:
+
+        >>> @deprecated("old_arg",
+        ...             "another_old_arg",
+        ...             suggested={"old_arg": "new_arg"},
+        ...             additional_message="Usage of 'old_arg' is discouraged.")
+        )
+        ...def my_func(old_arg, another_old_arg, new_arg=None):
+        ...   pass
+        >>> my_func("foo")
+        >>> # DeprecationWarning: Argument 'old_arg'
+        ... # in function `my_func` is deprecated and
+        ... # will be removed in future versions.
+        ... # Use 'new_arg' instead.
+        ... # Usage of 'old_arg' is discouraged.
+
+    @type args: str
+    @param args: The names of the deprecated parameters.
+    @type suggested: Dict[str, str]
+    @param suggested: Suggested replacement parameters.
+    @type additional_message: str
+    @param additional_message: Additional message to display.
+        If provided, it will be appended to the warning message.
+    @type altogether: bool
+    @param altogether: If True, the whole function is
+        marked as deprecated. Defaults to False.
+    """
+
+    def decorator(func):
+        @wraps(func)
+        def wrapper(*f_args, **f_kwargs):
+            fname = func.__name__
+            if altogether:
+                msg = f"'{fname}' is deprecated and will be removed in future versions."
+                if additional_message:
+                    msg += f" {additional_message}"
+                warnings.warn(msg, DeprecationWarning, stacklevel=2)
+
+            if args:
+                for arg in args:
+                    if arg in f_kwargs:
+                        replacement = suggested.get(arg) if suggested else None
+                        msg = (
+                            f"Argument '{arg}' in function '{fname}' "
+                            "is deprecated and will be removed in "
+                            "future versions."
+                        )
+                        if replacement:
+                            msg += f" Use '{replacement}' instead."
+                        if additional_message:
+                            msg += f" {additional_message}"
+                        warnings.warn(msg, DeprecationWarning, stacklevel=2)
+
+            return func(*f_args, **f_kwargs)
+
+        return wrapper
+
+    return decorator
