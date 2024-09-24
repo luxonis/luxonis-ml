631f8c8d6c0563e9059463f65ccfe32dde8076af
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2024-09-24 23:24:27 +0200

    modified guard_extras to raise an error instead of exit

--- luxonis_ml/guard_extras.py
+++ luxonis_ml/guard_extras.py
@@ -1,5 +1,4 @@
 import logging
-import sys
 from contextlib import contextmanager
 
 logger = logging.getLogger(__name__)
@@ -9,13 +8,12 @@ logger = logging.getLogger(__name__)
 def guard_missing_extra(name: str):
     try:
         yield
-    except ImportError:
-        logger.exception(
+    except ImportError as e:
+        raise ImportError(
             f"Error importing `luxonis-ml.{name}`. This can mean that some of the dependencies of `luxonis-ml[{name}]` are not installed. "
             f"Ensure you installed the package with the `[{name}]` or `[all]` extra specified. "
             f"Use `pip install luxonis-ml[{name}]` to install dependencies for the `{name}` submodule."
-        )
-        sys.exit(1)
+        ) from e
 
 
 __all__ = ["guard_missing_extra"]
