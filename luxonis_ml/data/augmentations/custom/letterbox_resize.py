fixed incorrect scaling of bboxes and keypoints in mixup

luxonis_ml/data/augmentations/custom/letterbox_resize.py
@@ -3,7 +3,6 @@ from typing import Any, Dict, Tuple
 import albumentations as A
 import cv2
 import numpy as np
-from albumentations.core.bbox_utils import denormalize_bboxes, normalize_bboxes
 from typing_extensions import override
 
 from luxonis_ml.data.utils.visualizations import resolve_color
@@ -66,20 +65,9 @@ class LetterboxResize(A.DualTransform):
         """
 
         params = super().update_params(params, **kwargs)
-
-        height = params["rows"]
-        width = params["cols"]
-
-        ratio = min(self._height / height, self._width / width)
-        new_height = int(height * ratio)
-        new_width = int(width * ratio)
-
-        # only supports center alignment
-        pad_top = (self._height - new_height) // 2
-        pad_bottom = pad_top
-
-        pad_left = (self._width - new_width) // 2
-        pad_right = pad_left
+        pad_top, pad_bottom, pad_left, pad_right = self.compute_padding(
+            params["rows"], params["cols"], self._height, self._width
+        )
 
         params.update(
             {
@@ -91,6 +79,36 @@ class LetterboxResize(A.DualTransform):
         )
 
         return params
+
+    @staticmethod
+    def compute_padding(
+        orig_height: int, orig_width: int, out_height: int, out_width: int
+    ) -> Tuple[int, int, int, int]:
+        """Computes the padding required to resize an image to a
+        letterbox format.
+
+        @type orig_height: int
+        @param orig_height: Original height of the image.
+        @type orig_width: int
+        @param orig_width: Original width of the image.
+        @type out_height: int
+        @param out_height: Desired height of the output.
+        @type out_width: int
+        @param out_width: Desired width of the output.
+        @rtype: Tuple[int, int, int, int]
+        @return: Padding values for the top, bottom, left and right
+            sides of the image.
+        """
+        ratio = min(out_height / orig_height, out_width / orig_width)
+        new_height = int(orig_height * ratio)
+        new_width = int(orig_width * ratio)
+
+        pad_top = (out_height - new_height) // 2
+        pad_bottom = pad_top
+
+        pad_left = (out_width - new_width) // 2
+        pad_right = pad_left
+        return pad_top, pad_bottom, pad_left, pad_right
 
     @override
     def apply(
@@ -195,20 +213,31 @@ class LetterboxResize(A.DualTransform):
         if bbox.shape[0] == 0:
             return bbox
 
-        bbox = denormalize_bboxes(
-            bbox,
-            (
-                self._height - pad_top - pad_bottom,
-                self._width - pad_left - pad_right,
-            ),
+        pad_left_norm = pad_left / self._width
+        pad_right_norm = pad_right / self._width
+        pad_top_norm = pad_top / self._height
+        pad_bottom_norm = pad_bottom / self._height
+
+        bbox[:, [0, 2]] *= 1 - pad_left_norm - pad_right_norm
+        bbox[:, [0, 2]] += pad_left_norm
+
+        bbox[:, [1, 3]] *= 1 - pad_top_norm - pad_bottom_norm
+        bbox[:, [1, 3]] += pad_top_norm
+
+        np.clip(
+            bbox[:, [0, 2]],
+            pad_left_norm,
+            1 - pad_right_norm,
+            out=bbox[:, [0, 2]],
         )
-        bbox[..., :4] += np.array([pad_left, pad_top] * 2)
-        bbox[..., :4] = bbox[..., :4].clip(
-            min=[pad_left, pad_top] * 2,
-            max=[self._width - pad_left, self._height - pad_top] * 2,
+        np.clip(
+            bbox[:, [1, 3]],
+            pad_top_norm,
+            1 - pad_bottom_norm,
+            out=bbox[:, [1, 3]],
         )
 
-        return normalize_bboxes(bbox, (self._height, self._width))
+        return bbox
 
     @override
     def apply_to_keypoints(
@@ -263,22 +292,6 @@ class LetterboxResize(A.DualTransform):
         keypoint[out_of_bounds_x | out_of_bounds_y, :2] = -1
 
         return keypoint
-
-    @override
-    def get_transform_init_args_names(self) -> Tuple[str, ...]:
-        """Gets the default arguments for the letterbox augmentation.
-
-        @rtype: Tuple[str, ...]
-        @return: The string keywords of the arguments.
-        """
-
-        return (
-            "height",
-            "width",
-            "interpolation",
-            "border_value",
-            "mask_value",
-        )
 
     def _apply_to_image_data(
         self,

luxonis_ml/data/augmentations/custom/mixup.py
@@ -5,7 +5,8 @@ import cv2
 import numpy as np
 from typing_extensions import override
 
-from ..batch_transform import BatchBasedTransform
+from luxonis_ml.data.augmentations.batch_transform import BatchBasedTransform
+from luxonis_ml.data.augmentations.custom import LetterboxResize
 
 
 class MixUp(BatchBasedTransform):
@@ -26,6 +27,7 @@ class MixUp(BatchBasedTransform):
         super().__init__(batch_size=2, p=p)
 
         self.alpha = alpha if isinstance(alpha, tuple) else (alpha, alpha)
+        self.letterbox = LetterboxResize(0, 0)
         self._check_alpha()
 
     def _check_alpha(self) -> None:
@@ -34,15 +36,6 @@ class MixUp(BatchBasedTransform):
 
         if self.alpha[0] > self.alpha[1]:
             raise ValueError("Alpha range must be in ascending order.")
-
-    @override
-    def get_transform_init_args_names(self) -> Tuple[str, ...]:
-        """Gets the default arguments for the mixup augmentation.
-
-        @rtype: Tuple[str, ...]
-        @return: The string keywords of the arguments.
-        """
-        return ("alpha",)
 
     @override
     def apply(
@@ -61,15 +54,19 @@ class MixUp(BatchBasedTransform):
         @rtype: List[np.ndarray]
         @return: List of transformed images.
         """
-        image1 = image_batch[0]
-        image2 = cv2.resize(
-            image_batch[1], (image_shapes[0][1], image_shapes[0][0])
+        alpha = random.uniform(self.alpha[0], self.alpha[1])
+
+        padding = self._update_letterbox_params(image_shapes)
+        self.letterbox._image_fill_value = (
+            int(255 * alpha),
+            int(255 * alpha),
+            int(255 * alpha),
         )
 
-        curr_alpha = random.uniform(
-            max(self.alpha[0], 0), min(self.alpha[1], 1)
-        )
-        return cv2.addWeighted(image1, curr_alpha, image2, 1 - curr_alpha, 0.0)
+        image1 = image_batch[0]
+        image2 = self.letterbox.apply(image_batch[1], *padding)
+
+        return cv2.addWeighted(image1, alpha, image2, 1 - alpha, 0.0)
 
     @override
     def apply_to_mask(
@@ -88,22 +85,19 @@ class MixUp(BatchBasedTransform):
         @rtype: List[np.ndarray]
         @return: List of transformed masks.
         """
+        padding = self._update_letterbox_params(image_shapes)
         mask1 = mask_batch[0]
-        mask2 = cv2.resize(
-            mask_batch[1],
-            (image_shapes[0][1], image_shapes[0][0]),
-            interpolation=cv2.INTER_NEAREST,
-        )
-        out_mask = mask1 + mask2
-        # if masks intersect keep the one from the first image
-        mask_inter = mask1 > 0
-        out_mask[mask_inter] = mask1[mask_inter]
-
-        return out_mask
+        mask2 = self.letterbox.apply_to_mask(mask_batch[1], *padding)
+        return np.minimum(mask1 + mask2, 1)
 
     @override
     def apply_to_bboxes(
-        self, bboxes_batch: List[np.ndarray], **_
+        self,
+        bboxes_batch: List[np.ndarray],
+        image_shapes: List[Tuple[int, int]],
+        rows: int,
+        cols: int,
+        **_,
     ) -> np.ndarray:
         """Applies the transformation to a batch of bboxes.
 
@@ -118,11 +112,19 @@ class MixUp(BatchBasedTransform):
             if bbox.shape[1] == 4:
                 bboxes_batch[i] = np.zeros((0, 6), dtype=bbox.dtype)
 
+        padding = self._update_letterbox_params(image_shapes)
+        bboxes_batch[1] = self.letterbox.apply_to_bboxes(
+            bboxes_batch[1], *padding, rows=rows, cols=cols
+        )
+
         return np.concatenate(bboxes_batch, axis=0)
 
     @override
     def apply_to_keypoints(
-        self, keypoints_batch: List[np.ndarray], **_
+        self,
+        keypoints_batch: List[np.ndarray],
+        image_shapes: List[Tuple[int, int]],
+        **_,
     ) -> np.ndarray:
         """Applies the transformation to a batch of keypoints.
 
@@ -132,6 +134,14 @@ class MixUp(BatchBasedTransform):
         @rtype: np.ndarray
         @return: Transformed keypoints.
         """
+        padding = self._update_letterbox_params(image_shapes)
+        rows, cols = image_shapes[1]
+        keypoints_batch[1] = self.letterbox.apply_to_keypoints(
+            keypoints_batch[1],
+            *padding,
+            rows=rows,
+            cols=cols,
+        )
         return np.concatenate(keypoints_batch, axis=0)
 
     @override
@@ -146,5 +156,17 @@ class MixUp(BatchBasedTransform):
             targets.
         @rtype: Dict[str, Any]
         """
+        params = super().get_params_dependent_on_data(params, data)
         image_batch = data["image"]
         return {"image_shapes": [image.shape[:2] for image in image_batch]}
+
+    def _update_letterbox_params(
+        self, image_shapes: List[Tuple[int, int]]
+    ) -> Tuple[int, int, int, int]:
+        out_height, out_width = image_shapes[0]
+        orig_height, orig_width = image_shapes[1]
+        self.letterbox._height = out_height
+        self.letterbox._width = out_width
+        return LetterboxResize.compute_padding(
+            orig_height, orig_width, out_height, out_width
+        )