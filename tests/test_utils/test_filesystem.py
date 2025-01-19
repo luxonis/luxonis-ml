920460f9abfb15e75504271f34216d61483a3126
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-01-19 18:02:49 -0500

    simplified

--- tests/test_utils/test_filesystem.py
+++ tests/test_utils/test_filesystem.py
@@ -15,9 +15,9 @@ PROTOCOLS = ["gcs", "s3"]
 
 
 @pytest.fixture
-def protocol_tempdir(tempdir: Path, randint: int, request: SubRequest) -> Path:
+def protocol_tempdir(tempdir: Path, request: SubRequest) -> Path:
     protocol = request.getfixturevalue("protocol")
-    tempdir = tempdir / protocol / str(randint)
+    tempdir = tempdir / protocol
     tempdir.mkdir(parents=True)
     return tempdir
 
@@ -40,7 +40,9 @@ def local_dir(protocol_tempdir: Path, randint: int) -> Path:
         file_path.write_text(f"file {randint}")
 
     (dir_path / "nested").mkdir()
-    (dir_path / "nested" / "file.txt").write_text(f"nested file {randint}")
+    (dir_path / "nested" / "nested_file.txt").write_text(
+        f"nested file {randint}"
+    )
 
     return dir_path
 
@@ -58,32 +60,13 @@ def fs(protocol: str, python_version: str, platform_name: str):
     yield LuxonisFileSystem(url_path)
 
 
-def test_protocol():
-    assert _get_protocol_and_path("foo://bar/baz") == ("foo", "bar/baz")
-    assert _get_protocol_and_path("gs://foo/bar") == ("gcs", "foo/bar")
-    assert _get_protocol_and_path("local_path/to/file") == (
-        "file",
-        "local_path/to/file",
-    )
-
-    with pytest.raises(ValueError):
-        LuxonisFileSystem("foo://bar")
-
-
-def test_fail(tempdir: Path, randint: int):
-    file = tempdir / f"file_{randint}.txt"
-    file.write_text("test")
-    with pytest.raises(ValueError):
-        LuxonisFileSystem(str(local_file), allow_local=False)
-
-
-def test_single_file(
+def test_file(
     fs: LuxonisFileSystem,
     local_file: Path,
     subtests: SubTests,
     protocol_tempdir: Path,
 ):
-    uploaded_file = f"test_upload_{local_file.name}"
+    uploaded_file = f"upload_{local_file.name}"
     if fs.exists(uploaded_file):  # pragma: no cover
         fs.delete_file(uploaded_file)
 
@@ -103,89 +86,6 @@ def test_single_file(
     with subtests.test("delete"):
         fs.delete_file(uploaded_file)
         assert not fs.exists(uploaded_file)
-
-
-def test_directory(
-    fs: LuxonisFileSystem,
-    local_dir: Path,
-    subtests: SubTests,
-    protocol_tempdir: Path,
-):
-    uploaded_dir = f"dir_upload_{local_dir.name}"
-    if fs.exists(uploaded_dir):  # pragma: no cover
-        fs.delete_dir(uploaded_dir)
-
-    assert not fs.exists(uploaded_dir)
-
-    with subtests.test("upload_name"):
-        fs.put_dir(local_dir, uploaded_dir)
-        assert fs.exists(uploaded_dir)
-        assert fs.is_directory(uploaded_dir)
-
-    with subtests.test("upload_list"):
-        fs.put_dir(
-            [str(p) for p in local_dir.iterdir()],
-            f"{uploaded_dir}_list",
-        )
-        assert fs.exists(f"{uploaded_dir}_list")
-        assert fs.is_directory(f"{uploaded_dir}_list")
-
-    with subtests.test("download_name"):
-        dir_path = fs.get_dir(
-            uploaded_dir, protocol_tempdir / f"dowload_{uploaded_dir}_name"
-        )
-        compare_directories(dir_path, local_dir)
-
-    with subtests.test("download_list"):
-        dir_path = fs.get_dir(
-            [f"{uploaded_dir}/file_{i}.txt" for i in range(5)]
-            + [f"{uploaded_dir}/nested/file.txt"],
-            protocol_tempdir / f"dowload_{uploaded_dir}_list",
-        )
-        compare_directories(dir_path, local_dir, flat=True)
-
-
-def test_walk_dir(fs: LuxonisFileSystem, local_dir: Path, subtests: SubTests):
-    uploaded_dir = f"dir_upload_{local_dir.name}"
-
-    if fs.exists(uploaded_dir):  # pragma: no cover
-        fs.delete_dir(uploaded_dir)
-
-    fs.put_dir(local_dir, uploaded_dir)
-
-    def walk(**kwargs):
-        return sorted(
-            [
-                path.lstrip(f"{uploaded_dir}/")
-                for path in fs.walk_dir(uploaded_dir, **kwargs)
-            ]
-        )
-
-    with subtests.test("recursive"):
-        files = walk(recursive=True, typ="file")
-
-        assert files == sorted(
-            [f"file_{i}.txt" for i in range(5)] + ["nested/file.txt"]
-        )
-
-    with subtests.test("all"):
-        files = walk(recursive=False, typ="all")
-
-        assert files == sorted(
-            [f"file_{i}.txt" for i in range(5)] + ["nested"]
-        )
-
-    with subtests.test("directories"):
-        files = walk(recursive=False, typ="directory")
-        assert files == ["nested"]
-
-
-def test_bytes(fs: LuxonisFileSystem, randint):
-    bytes_file = f"bytes_test_{randint}.txt"
-    fs.put_bytes(f"bytes test {randint}".encode(), bytes_file)
-    assert fs.exists(bytes_file)
-    buffer = fs.read_to_byte_buffer(bytes_file)
-    assert buffer.read() == f"bytes test {randint}".encode()
 
 
 def test_static_file(
@@ -209,6 +109,44 @@ def test_static_file(
         assert file.read_text() == local_file.read_text()
 
 
+def test_directory(
+    fs: LuxonisFileSystem,
+    local_dir: Path,
+    subtests: SubTests,
+    protocol_tempdir: Path,
+):
+    uploaded_dir = f"upload_{local_dir.name}"
+    assert not fs.exists(uploaded_dir)
+
+    with subtests.test("upload_name"):
+        fs.put_dir(local_dir, uploaded_dir)
+        assert fs.exists(uploaded_dir)
+        assert fs.is_directory(uploaded_dir)
+
+    with subtests.test("upload_list"):
+        fs.put_dir(
+            [str(p) for p in local_dir.iterdir()],
+            f"{uploaded_dir}_list",
+        )
+        assert fs.exists(f"{uploaded_dir}_list")
+        assert fs.is_directory(f"{uploaded_dir}_list")
+
+    with subtests.test("download_name"):
+        dir_path = fs.get_dir(
+            uploaded_dir, protocol_tempdir / f"dowload_{local_dir.name}_name"
+        )
+        compare_directories(dir_path, local_dir)
+    compare_directories(dir_path, local_dir)
+
+    with subtests.test("download_list"):
+        dir_path = fs.get_dir(
+            [f"{uploaded_dir}/file_{i}.txt" for i in range(5)]
+            + [f"{uploaded_dir}/nested/nested_file.txt"],
+            protocol_tempdir / f"dowload_{local_dir.name}_list",
+        )
+        compare_directories(dir_path, local_dir, flat=True)
+
+
 def test_static_directory(
     fs: LuxonisFileSystem,
     local_dir: Path,
@@ -229,6 +167,68 @@ def test_static_directory(
         compare_directories(dir, local_dir)
 
 
+def test_walk_dir(fs: LuxonisFileSystem, local_dir: Path, subtests: SubTests):
+    uploaded_dir = f"dir_upload_{local_dir.name}"
+
+    if fs.exists(uploaded_dir):  # pragma: no cover
+        fs.delete_dir(uploaded_dir)
+
+    fs.put_dir(local_dir, uploaded_dir)
+
+    def walk(**kwargs):
+        return sorted(
+            [
+                path.lstrip(f"{uploaded_dir}/")
+                for path in fs.walk_dir(uploaded_dir, **kwargs)
+            ]
+        )
+
+    with subtests.test("recursive"):
+        files = walk(recursive=True, typ="file")
+
+        assert files == sorted(
+            [f"file_{i}.txt" for i in range(5)] + ["nested/nested_file.txt"]
+        )
+
+    with subtests.test("all"):
+        files = walk(recursive=False, typ="all")
+
+        assert files == sorted(
+            [f"file_{i}.txt" for i in range(5)] + ["nested"]
+        )
+
+    with subtests.test("directories"):
+        files = walk(recursive=False, typ="directory")
+        assert files == ["nested"]
+
+
+def test_protocol():
+    assert _get_protocol_and_path("foo://bar/baz") == ("foo", "bar/baz")
+    assert _get_protocol_and_path("gs://foo/bar") == ("gcs", "foo/bar")
+    assert _get_protocol_and_path("local_path/to/file") == (
+        "file",
+        "local_path/to/file",
+    )
+
+    with pytest.raises(ValueError):
+        LuxonisFileSystem("foo://bar")
+
+
+def test_fail(tempdir: Path, randint: int):
+    file = tempdir / f"file_{randint}.txt"
+    file.write_text("test")
+    with pytest.raises(ValueError):
+        LuxonisFileSystem(str(local_file), allow_local=False)
+
+
+def test_bytes(fs: LuxonisFileSystem, randint):
+    bytes_file = f"bytes_test_{randint}.txt"
+    fs.put_bytes(f"bytes test {randint}".encode(), bytes_file)
+    assert fs.exists(bytes_file)
+    buffer = fs.read_to_byte_buffer(bytes_file)
+    assert buffer.read() == f"bytes test {randint}".encode()
+
+
 def compare_directories(
     dir_path: Path, orig_dir_path: Path, flat: bool = False
 ):
@@ -241,15 +241,15 @@ def compare_directories(
             == (orig_dir_path / f"file_{i}.txt").read_text()
         )
     if flat:
-        file_path = "file.txt"
+        file_path = "nested_file.txt"
     else:
-        file_path = "nested/file.txt"
+        file_path = "nested/nested_file.txt"
     nested_file = dir_path / file_path
 
     assert nested_file.exists()
     assert (
         nested_file.read_text()
-        == (orig_dir_path / "nested" / "file.txt").read_text()
+        == (orig_dir_path / "nested" / "nested_file.txt").read_text()
     )
 
 
