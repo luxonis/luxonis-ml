7ce63a136b0fde02d297f08a28c0d87cefb18f0c
Martin Kozlovský <martin.kozlovsky@luxonis.com>
2025-03-06 19:04:25 +0100

    Deprecate `DatasetRecord.task` (#270)

--- luxonis_ml/data/datasets/annotation.py
+++ luxonis_ml/data/datasets/annotation.py
@@ -534,7 +534,7 @@ class ArrayAnnotation(Annotation):
 class DatasetRecord(BaseModelExtraForbid):
     files: Dict[str, FilePath]
     annotation: Optional[Detection] = None
-    task: str = ""
+    task_name: str = ""
 
     @property
     def file(self) -> FilePath:
@@ -543,9 +543,20 @@ class DatasetRecord(BaseModelExtraForbid):
         return next(iter(self.files.values()))
 
     @model_validator(mode="after")
-    def validate_task_name(self) -> Self:
-        check_valid_identifier(self.task, label="Task name")
+    def validate_task_name_valid_identifier(self) -> Self:
+        check_valid_identifier(self.task_name, label="Task name")
         return self
+
+    @model_validator(mode="before")
+    @classmethod
+    def validate_task_name(cls, values: Dict[str, Any]) -> Dict[str, Any]:
+        if "task" in values:
+            warnings.warn(
+                "The 'task' field is deprecated. Use 'task_name' instead.",
+                stacklevel=2,
+            )
+            values["task_name"] = values.pop("task")
+        return values
 
     @model_validator(mode="before")
     @classmethod
@@ -562,7 +573,7 @@ class DatasetRecord(BaseModelExtraForbid):
         @rtype: L{ParquetDict}
         @return: A dictionary of annotation data.
         """
-        yield from self._to_parquet_rows(self.annotation, self.task)
+        yield from self._to_parquet_rows(self.annotation, self.task_name)
 
     def _to_parquet_rows(
         self, annotation: Optional[Detection], task_name: str
