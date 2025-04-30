6e264cac7db0d276cac34965ad5a5189be26fb5e
Jernej Sabadin <jernej14sabadin@gmail.com>
2025-04-30 17:24:00 +0200

    add docs

--- tests/test_data/test_augmentations/test_special.py
+++ tests/test_data/test_augmentations/test_special.py
@@ -77,5 +77,5 @@ def test_skip_augmentations():
         t.__class__.__name__ for t in augmentations.batch_transform.transforms
     ]
 
-    assert spatial_transform_names == ["Rotate"]
+    assert spatial_transform_names == ["Perspective", "Rotate"]
     assert batched_transform_names == ["Mosaic4"]
