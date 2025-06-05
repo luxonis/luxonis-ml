a350356ff06c947e073b1266076f01113aff81ed
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-06-05 22:12:11 +0200

    disabling albumentation logs

--- luxonis_ml/__init__.py
+++ luxonis_ml/__init__.py
@@ -1,7 +1,11 @@
 __version__ = "0.7.0"
 
+import os
+
 from .utils.environ import environ
 from .utils.logging import setup_logging
 
 if not environ.LUXONISML_DISABLE_SETUP_LOGGING:
     setup_logging()
+
+os.environ["NO_ALBUMENTATIONS_UPDATE"] = "1"
