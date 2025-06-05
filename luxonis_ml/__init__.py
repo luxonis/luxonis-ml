f1405d8f46bbd59e44740966bd97e0eeebca894b
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-06-05 22:13:46 +0200

    check for existing value

--- luxonis_ml/__init__.py
+++ luxonis_ml/__init__.py
@@ -8,4 +8,5 @@ from .utils.logging import setup_logging
 if not environ.LUXONISML_DISABLE_SETUP_LOGGING:
     setup_logging()
 
-os.environ["NO_ALBUMENTATIONS_UPDATE"] = "1"
+if "NO_ALBUMENTATIONS_UPDATE" not in os.environ:
+    os.environ["NO_ALBUMENTATIONS_UPDATE"] = "1"
