be79c8e9bbadb1d13c1cf7861e96405f24aa71fe
Jernej Sabadin <116955183+JSabadin@users.noreply.github.com>
2025-07-17 19:33:19 +0200

    Make `bbox_area_threshold` and `min_bbox_visibility` configurable (#348)
    Co-authored-by: Nikita Sokovnin <49622375+sokovninn@users.noreply.github.com>

--- luxonis_ml/data/loaders/luxonis_loader.py
+++ luxonis_ml/data/loaders/luxonis_loader.py
@@ -48,6 +48,8 @@ class LuxonisLoader(BaseLoader):
         exclude_empty_annotations: bool = False,
         color_space: Literal["RGB", "BGR", "GRAY"] = "RGB",
         seed: int | None = None,
+        min_bbox_visibility: float = 0.0,
+        bbox_area_threshold: float = 0.0004,
         *,
         keep_categorical_as_strings: bool = False,
         update_mode: UpdateMode | Literal["all", "missing"] = UpdateMode.ALL,
@@ -95,6 +97,11 @@ class LuxonisLoader(BaseLoader):
             to C{"RGB"}.
         @type seed: Optional[int]
         @param seed: The random seed to use for the augmentations.
+        @type min_bbox_visibility: float
+        @param min_bbox_visibility: Minimum fraction of the original bounding box that must remain visible after augmentation.
+        @type bbox_area_threshold: float
+        @param bbox_area_threshold: Minimum area threshold for bounding boxes to be considered valid. In the range [0, 1].
+            Default is 0.0004, which corresponds to a small area threshold to remove invalid bboxes and respective keypoints.
         @type exclude_empty_annotations: bool
         @param exclude_empty_annotations: Whether to exclude
             empty annotations from the final label dictionary.
@@ -218,6 +225,8 @@ class LuxonisLoader(BaseLoader):
             width,
             keep_aspect_ratio,
             seed,
+            min_bbox_visibility,
+            bbox_area_threshold,
         )
 
     @override
@@ -434,6 +443,8 @@ class LuxonisLoader(BaseLoader):
         width: int | None,
         keep_aspect_ratio: bool,
         seed: int | None = None,
+        min_bbox_visibility: float = 0.0,
+        bbox_area_threshold: float = 0.0004,
     ) -> AugmentationEngine | None:
         if isinstance(augmentation_config, PathType):
             with open(augmentation_config) as file:
@@ -475,6 +486,8 @@ class LuxonisLoader(BaseLoader):
             keep_aspect_ratio=keep_aspect_ratio,
             is_validation_pipeline="train" not in self.view,
             seed=seed,
+            min_bbox_visibility=min_bbox_visibility,
+            bbox_area_threshold=bbox_area_threshold,
         )
 
     def _precompute_image_paths(self) -> None:
