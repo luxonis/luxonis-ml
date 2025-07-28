04d83969402a29ce5acf3a8ff636b750bb4eb861
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2025-07-28 05:22:33 +0200

    fix tests

--- tests/test_utils/test_environ.py
+++ tests/test_utils/test_environ.py
@@ -10,28 +10,18 @@ from luxonis_ml.utils.environ import Environ
 @pytest.fixture
 def dotenv_file(tempdir: Path) -> Generator[Path]:
     path = tempdir / "dotenv"
-    path.write_text(
-        "AWS_ACCESS_KEY_ID=example_access_key\n"
-        "AWS_SECRET_ACCESS_KEY=example_secret_key\n"
-        "AWS_S3_ENDPOINT_URL=http://example.com\n"
-    )
+    path.write_text("POSTGRES_USER=test\nPOSTGRES_PASSWORD=pass\n")
     yield path
     path.unlink(missing_ok=True)
 
 
-def test_environ(dotenv_file: Path, monkeypatch: pytest.MonkeyPatch):
+def test_environ(dotenv_file: Path):
     environ = Environ(
         POSTGRES_USER="user",
         _env_file=dotenv_file,  # type: ignore
     )
     assert environ.POSTGRES_USER == "user"
-    assert isinstance(environ.AWS_ACCESS_KEY_ID, SecretStr)
-    assert isinstance(environ.AWS_SECRET_ACCESS_KEY, SecretStr)
-    assert (
-        environ.AWS_SECRET_ACCESS_KEY.get_secret_value()
-        == "example_secret_key"
-    )
-    assert environ.AWS_S3_ENDPOINT_URL == "http://example.com"
-    assert environ.AWS_ACCESS_KEY_ID.get_secret_value() == "example_access_key"
+    assert isinstance(environ.POSTGRES_PASSWORD, SecretStr)
+    assert environ.POSTGRES_PASSWORD.get_secret_value() == "pass"
 
     assert environ.model_dump() == {}
