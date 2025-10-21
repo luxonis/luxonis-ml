6983d85b52e87b90229514043ff3732bd8d82a05
Dani Rogmans <danirogmans@gmail.com>
2025-10-21 15:14:01 +0200

    add unfound Flip back to skipped augmentations to keypoints

--- luxonis_ml/data/augmentations/albumentations_engine.py
+++ luxonis_ml/data/augmentations/albumentations_engine.py
@@ -241,6 +241,33 @@ class AlbumentationsEngine(AugmentationEngine, register_name="albumentations"):
                 ...
     """
 
+    def _should_skip_augmentation(
+        self, config_item: dict[str, Any], available_target_types: set
+    ) -> bool:
+        skip_rules = {
+            "keypoints": ["Flip"],
+        }
+        augmentation_name = config_item["name"]
+        skipped_for = [
+            target_type
+            for target_type, skip_list in skip_rules.items()
+            if target_type in available_target_types
+            and augmentation_name in skip_list
+        ]
+        if skipped_for:
+            extra_msg = ""
+            if "keypoints" in skipped_for and augmentation_name in [
+                "HorizontalFlip",
+                "VerticalFlip",
+                "Flip",
+            ]:
+                extra_msg = " For keypoints, please use 'HorizontalSymetricKeypointsFlip' or 'VerticalSymetricKeypointsFlip'."
+            logger.warning(
+                f"Skipping augmentation '{augmentation_name}' due to known issues for {skipped_for} target types. {extra_msg}"
+            )
+            return True
+        return False
+
     @override
     def __init__(
         self,
@@ -337,6 +364,16 @@ class AlbumentationsEngine(AugmentationEngine, register_name="albumentations"):
 
         if is_validation_pipeline:
             config = (a for a in config if a["name"] == "Normalize")
+
+        available_target_types = set(self.targets.values())
+
+        for config_item in config:
+            if self._should_skip_augmentation(
+                config_item, available_target_types
+            ):
+                continue
+
+            cfg = AlbumentationConfigItem(**config_item)  # type: ignore
 
         for config_item in config:
             cfg = AlbumentationConfigItem(**config_item)  # type: ignore
