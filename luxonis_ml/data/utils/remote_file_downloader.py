9feb9f90069310b4d69846719b641a339c8e89cc
Dani Rogmans <danirogmans@gmail.com>
2026-05-18 12:57:47 +0200

    move image format spec at the top of file

--- luxonis_ml/data/utils/remote_file_downloader.py
+++ luxonis_ml/data/utils/remote_file_downloader.py
@@ -8,6 +8,40 @@ from PIL import Image, UnidentifiedImageError
 
 from luxonis_ml.utils.filesystem import LuxonisFileSystem
 
+ALLOWED_IMAGE_SUFFIXES_BY_FORMAT = {
+    "JPEG": frozenset({".jpeg", ".jpg"}),
+    "PNG": frozenset({".png"}),
+    "WEBP": frozenset({".webp"}),
+    "BMP": frozenset({".bmp"}),
+    "TIFF": frozenset({".tif", ".tiff"}),
+    "HEIC": frozenset({".heic"}),
+    "AVIF": frozenset({".avif"}),
+    "JP2": frozenset({".jp2"}),
+    "DNG": frozenset({".dng"}),
+    "MPO": frozenset({".mpo"}),
+}
+ALLOWED_IMAGE_SUFFIXES = frozenset(
+    suffix
+    for suffixes in ALLOWED_IMAGE_SUFFIXES_BY_FORMAT.values()
+    for suffix in suffixes
+)
+ALLOWED_PILLOW_FORMATS_BY_FORMAT = {
+    "JPEG": frozenset({"JPEG"}),
+    "PNG": frozenset({"PNG"}),
+    "WEBP": frozenset({"WEBP"}),
+    "BMP": frozenset({"BMP"}),
+    "TIFF": frozenset({"TIFF"}),
+    "HEIC": frozenset({"HEIC", "HEIF"}),
+    "AVIF": frozenset({"AVIF"}),
+    "JP2": frozenset({"JP2", "JPEG2000"}),
+    "DNG": frozenset({"DNG", "TIFF"}),
+    "MPO": frozenset({"MPO"}),
+}
+FS_REMOTE_URL_SCHEMES = frozenset({"gcs", "gs", "s3"})
+ALLOWED_REMOTE_URL_SCHEMES = frozenset(
+    {"file", "http", "https", *FS_REMOTE_URL_SCHEMES}
+)
+
 
 class RemoteFileAPIClient(Protocol):
     def download(
@@ -188,38 +222,3 @@ def download_remote_file(
         timeout=timeout,
         validate_image=validate_image,
     )
-
-
-ALLOWED_IMAGE_SUFFIXES_BY_FORMAT = {
-    "JPEG": frozenset({".jpeg", ".jpg"}),
-    "PNG": frozenset({".png"}),
-    "WEBP": frozenset({".webp"}),
-    "BMP": frozenset({".bmp"}),
-    "TIFF": frozenset({".tif", ".tiff"}),
-    "HEIC": frozenset({".heic"}),
-    "AVIF": frozenset({".avif"}),
-    "JP2": frozenset({".jp2"}),
-    "DNG": frozenset({".dng"}),
-    "MPO": frozenset({".mpo"}),
-}
-ALLOWED_IMAGE_SUFFIXES = frozenset(
-    suffix
-    for suffixes in ALLOWED_IMAGE_SUFFIXES_BY_FORMAT.values()
-    for suffix in suffixes
-)
-ALLOWED_PILLOW_FORMATS_BY_FORMAT = {
-    "JPEG": frozenset({"JPEG"}),
-    "PNG": frozenset({"PNG"}),
-    "WEBP": frozenset({"WEBP"}),
-    "BMP": frozenset({"BMP"}),
-    "TIFF": frozenset({"TIFF"}),
-    "HEIC": frozenset({"HEIC", "HEIF"}),
-    "AVIF": frozenset({"AVIF"}),
-    "JP2": frozenset({"JP2", "JPEG2000"}),
-    "DNG": frozenset({"DNG", "TIFF"}),
-    "MPO": frozenset({"MPO"}),
-}
-FS_REMOTE_URL_SCHEMES = frozenset({"gcs", "gs", "s3"})
-ALLOWED_REMOTE_URL_SCHEMES = frozenset(
-    {"file", "http", "https", *FS_REMOTE_URL_SCHEMES}
-)
