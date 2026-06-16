bc3be95a27fb70ae3a363b25e031c5ac0e0d26e1
klemen1999 <klemenskrlj8@gmail.com>
2026-06-16 12:18:11 +0200

    Release v0.8.6

--- luxonis_ml/__init__.py
+++ luxonis_ml/__init__.py
@@ -2,7 +2,7 @@ from typing import Final
 
 from pydantic_extra_types.semantic_version import SemanticVersion
 
-__version__: Final[str] = "0.8.5"
+__version__: Final[str] = "0.8.6"
 __semver__: Final[SemanticVersion] = SemanticVersion.parse(__version__)
 
 import os
