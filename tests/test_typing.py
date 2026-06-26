08304e4dece4ec5f5554a6fab0b75ddad13e3fa2
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2026-06-26 06:18:53 +0200

    fix

--- tests/test_typing.py
+++ tests/test_typing.py
@@ -1,8 +1,4 @@
-import numpy as np
-
 from luxonis_ml.typing import (
-    LoaderOutput,
-    Params,
     all_not_none,
     any_not_none,
     check_type,
@@ -28,27 +24,3 @@ def test_check_type():
     assert not check_type(1, str)
     assert check_type([1, 2, 3], list[int])
     assert not check_type([1, 2, 3], list[str])
-
-
-def test_loader_output_tuple_compatibility():
-    image = np.empty((100, 100, 3), dtype=np.uint8)
-    labels = {"task/boundingbox": np.empty((5, 4), dtype=np.float32)}
-    metadata: Params = {"source": "camera-a"}
-
-    output = LoaderOutput({"image": image}, labels, metadata)
-
-    assert len(output) == 2
-    unpacked_image, unpacked_labels = output
-    assert unpacked_image is image
-    assert unpacked_labels is labels
-    assert output[0] is image
-    assert output[1] is labels
-    assert output.images == {"image": image}
-    assert output.labels is labels
-    assert output.metadata is metadata
-    assert output.image is image
-
-    right = np.empty((10, 10, 3), dtype=np.uint8)
-    multi_output = LoaderOutput({"left": image, "right": right}, labels, {})
-    assert multi_output[0] == multi_output.images
-    assert multi_output.image is image
