92f9e243e60e0d1add01c48102d98079579307e3
Martin Kozlovsky <martin.kozlovsky@luxonis.com>
2026-07-22 10:07:37 +0200

    ruff

--- tests/test_utils/test_filesystem_mlflow.py
+++ tests/test_utils/test_filesystem_mlflow.py
@@ -38,7 +38,7 @@ def _wait_for_mlflow_server(
             ) as response:
                 if response.status == 200:
                     return
-        except (URLError, TimeoutError, socket.timeout):
+        except (URLError, TimeoutError):
             time.sleep(0.2)
     process.terminate()
     raise TimeoutError(log_path.read_text())
