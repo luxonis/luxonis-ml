c7bf3d843509b7db0326893a55823bf07e7db2b4
klemen1999 <klemenskrlj8@gmail.com>
2026-07-29 21:39:40 +0200

    preserve parameterless batch transform outputs

--- luxonis_ml/data/augmentations/batch_compose.py
+++ luxonis_ml/data/augmentations/batch_compose.py
@@ -85,7 +85,10 @@ class BatchCompose(A.Compose):
                     i * transform.batch_size : (i + 1) * transform.batch_size
                 ]
 
-                if not transform.params:
+                returned_batched_input = isinstance(
+                    next(iter(data.values())), list
+                )
+                if returned_batched_input:
                     data = {key: value[0] for key, value in batch.items()}
                     new_indices.append(batch_indices[0])
                 else:
